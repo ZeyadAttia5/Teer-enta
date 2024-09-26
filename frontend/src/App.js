8d81c56b652622b4db5e76174d9981ef13238e64
helguindy <133847291+helguindy@users.noreply.github.com>
2024-09-26 19:34:06 +0300

    delete user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import DeleteUser from './components/deleteUser.js';
 
 function App() {
   return (
     <div className="App">
       App
+      <DeleteUser/>
     </div>
+    
   );
 }
 
