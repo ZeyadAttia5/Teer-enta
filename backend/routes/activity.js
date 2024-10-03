058f4f2768caa1a1424f21fbbe15ced690784f85
Mahmoud <mahmoudhanyhebishy@gmail.com>
2024-10-04 02:58:55 +0300

    sprint 2 middlewares added

--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const activityController = require("../controllers/activity");
+const itineraryController = require("../controllers/Itinerary");
 
 router.get("/", activityController.getActivities);
 
@@ -18,5 +19,6 @@ router.put("/update/:id", activityController.updateActivity);
 
 router.delete("/delete/:id", activityController.deleteActivity);
 
+router.patch("/flagInappropriate/:id" , activityController.flagInappropriate)
 
 module.exports = router;
