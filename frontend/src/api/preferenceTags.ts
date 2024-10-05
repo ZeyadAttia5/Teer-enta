baff4a9c4b61fe23bb23e65383e03bfe3257897c
Ahmed Said <ahmeddragon700@gmail.com>
2024-10-05 21:11:00 +0300

    Intial commit

--- frontend/src/api/preferenceTags.ts
+++ frontend/src/api/preferenceTags.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosResponse } from "axios";
 import { TPreferenceTag } from "../types/Itinerary/PreferenceTag";
-
-// import http from "./http";
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
+import http from "./http.ts";
 
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const token = localStorage.getItem("accessToken");
 
-export const getPreferenceTags = async (): Promise<TPreferenceTag[]> => {
-    const response = await axios.get<TPreferenceTag[]>(`${API_BASE_URL}/preferenceTag/`);
-    return response.data;
-};
+export const getPreferenceTags = async () =>
+  await axios.get<TPreferenceTag[]>(`${BASE_URL}/preferenceTag/`,);
+
+export const createPreferenceTag = async (preferenceTag: TPreferenceTag) =>
+    await http.post<TPreferenceTag>(`${BASE_URL}/preferenceTag/create`, preferenceTag);
+
---
+++ frontend/src/screens/PrefrenceTags.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrefrenceTags = () => {
+  return (
+    <div>PrefrenceTags</div>
+  )
+}
+
+export default PrefrenceTags
