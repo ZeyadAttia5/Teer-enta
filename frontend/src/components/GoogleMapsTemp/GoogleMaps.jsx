2cd9359147215cfd2a03b4d08db797058ec71ffc
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-10-06 11:41:48 +0300

    adding drag and choose on map functionality to map

--- frontend/src/components/GoogleMapsTemp/GoogleMaps.jsx
+++ frontend/src/components/GoogleMapsTemp/GoogleMaps.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
 const MapContainer = () => {
@@ -6,10 +6,22 @@ const MapContainer = () => {
         height: "400px",
         width: "100%",
     };
-
-    const defaultCenter = {
+    const initialPosition = {
         lat: 40.748817,  // Put latitude here
         lng: -73.985428, // Put longitude here
+    };
+
+    const [markerPosition, setMarkerPosition] = useState(initialPosition);
+    // Handle user click event on the map to move the marker
+    const handleMapClick = (event) => {
+        const newLat = event.latLng.lat();
+        const newLng = event.latLng.lng();
+
+        // Update the marker position based on user click
+        setMarkerPosition({
+            lat: newLat,
+            lng: newLng,
+        });
     };
 
     return (
@@ -18,9 +30,10 @@ const MapContainer = () => {
             <GoogleMap
                 mapContainerStyle={mapStyles}
                 zoom={13}
-                center={defaultCenter}
+                center={markerPosition}
+                onClick={handleMapClick}
             >
-                <MarkerF visible={true} position={defaultCenter} />
+                <MarkerF position={markerPosition} draggable={true} onDrag={handleMapClick}/>
             </GoogleMap>
         </LoadScript>
     );
