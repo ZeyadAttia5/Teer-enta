7ea1550508d7fa3ec33cec7b052d185e172b078c
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-10-07 00:28:37 +0300

    Fixed problem when google maps api is loaded multiple times

--- frontend/src/components/GoogleMaps/ViewLocation.jsx
+++ frontend/src/components/GoogleMaps/ViewLocation.jsx
@@ -1,14 +1,11 @@
-import React, {useState} from "react";
-import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import {GoogleMap, MarkerF, useJsApiLoader} from "@react-google-maps/api";
 
 /**
  * MapContainer component that displays a Google Map
  * and allows user to click on the map to set a marker
  * @param latitude initial latitude of marker and map
  * @param longitude initial longitude of marker and map
- * @param outputLocation callback function given by parent to output the location of the marker
- * function should take two inputs: latitude and longitude
- * @returns {Element}
+ * @returns {JSX.Element}
  * @constructor
  */
 const StaticMap = ({latitude = 1, longitude = 1}) => {
@@ -24,17 +21,22 @@ const StaticMap = ({latitude = 1, longitude = 1}) => {
     const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     const mapId = process.env.REACT_APP_GOOGLE_MAPS_MAP;
 
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: apiKey,
+        libraries: ['geometry', 'drawing'],
+        mapIds: [mapId]
+    });
     return (
-        <LoadScript googleMapsApiKey={apiKey}
-                    mapIds={[mapId]}>
-            <GoogleMap
+        <>
+            {isLoaded && <GoogleMap
                 mapContainerStyle={mapStyles}
                 zoom={13}
                 center={initialPosition}
             >
                 <MarkerF position={initialPosition}/>
-            </GoogleMap>
-        </LoadScript>
+            </GoogleMap>}
+        </>
     );
 };
 
