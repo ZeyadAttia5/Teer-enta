3fd5435bb11de1d978610b132abed9eb65f589dc
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-10-06 14:39:30 +0300

    Made googleMaps component take input initial location and callback function to provide the output

--- frontend/src/components/GoogleMapsTemp/GoogleMaps.jsx
+++ frontend/src/components/GoogleMapsTemp/GoogleMaps.jsx
@@ -1,23 +1,37 @@
 import React, {useState} from "react";
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
-const MapContainer = () => {
+/**
+ * MapContainer component that displays a Google Map
+ * and allows user to click on the map to set a marker
+ * @param initialPosition the initial position of the marker
+ * @param outputLocation callback function given by parent to output the location of the marker
+ * function should take two inputs: latitude and longitude
+ * @returns {Element}
+ * @constructor
+ */
+const MapContainer = ({initialPosition, outputLocation}) => {
     const mapStyles = {
         height: "400px",
         width: "100%",
     };
-    const initialPosition = {
-        lat: 40.748817,  // Put latitude here
-        lng: -73.985428, // Put longitude here
-    };
+    if(!initialPosition){
+        // Example input position
+        initialPosition = {
+            lat: 40.748817,  // Put latitude here
+            lng: -73.985428, // Put longitude here
+        };
+    }
 
     const [markerPosition, setMarkerPosition] = useState(initialPosition);
-    // Handle user click event on the map to move the marker
+
     const handleMapClick = (event) => {
         const newLat = event.latLng.lat();
         const newLng = event.latLng.lng();
 
-        // Update the marker position based on user click
+        console.log("New Latitude and Longitude: " + newLat + ", " + newLng);
+        outputLocation(newLat, newLng);
+        // Update the marker position based on user click location
         setMarkerPosition({
             lat: newLat,
             lng: newLng,
