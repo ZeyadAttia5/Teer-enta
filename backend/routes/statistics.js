da7d8a7d7618da4969cd365aef55725bf02c780c
Ahmed Said <ahmeddragon700@gmail.com>
2024-11-18 02:08:33 +0200

    Intial commit

--- backend/routes/statistics.js
+++ backend/routes/statistics.js
@@ -7,9 +7,9 @@ const router = express.Router();
 router.get('/totalUsers' ,statisticsController.getTotalUsers);
 router.get('/newUsersPerMonth',statisticsController.getNewUsersPerMonth);
 router.get('/report/adminRevenue',isAuth, statisticsController.getAdminRevenueReport);
-router.get('/report/itinerary',isAuth, statisticsController.getItineraryReport);
-router.get('/report/activity',isAuth, statisticsController.getActivityReport);
-router.get('/report/transportation',isAuth, statisticsController.getTransportationReport);
-router.get('/report/order',isAuth, statisticsController.getProductReport);
+router.get('/report/itinerary',isAuth, statisticsController.getItineraryReport);   // tourguide 
+router.get('/report/activity',isAuth, statisticsController.getActivityReport);  // adervisters
+router.get('/report/transportation',isAuth, statisticsController.getTransportationReport);  // advertiser
+router.get('/report/order',isAuth, statisticsController.getProductReport); // admin , seller
 
 module.exports = router;
