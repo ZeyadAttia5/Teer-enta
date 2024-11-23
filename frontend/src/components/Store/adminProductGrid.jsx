bf3e3330b93fd0f42a352ab0818cabd2b3b53bff
unknown <youssefmmagdy55@gmail.com>
2024-11-23 12:25:46 -0800

    .

--- frontend/src/components/Store/adminProductGrid.jsx
+++ frontend/src/components/Store/adminProductGrid.jsx
@@ -180,8 +180,8 @@ const AdminProductGrid = ({ setFlag }) => {
   {user && (user.userRole === "Admin" || user.userRole === "Seller") && (
     <Link to="/products/create">
       <Button
-        type="primary"
-        className="bg-first text-white flex items-center justify-center h-12 w-20 bg-no-repeat bg-center bg-[length:50%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
+        type="danger"
+        className="bg-second text-white hover:bg-black flex items-center justify-center  bg-no-repeat bg-center bg-[length:55%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
         style={{
           backgroundImage: `url(${addSign})`
         }}
@@ -195,8 +195,8 @@ const AdminProductGrid = ({ setFlag }) => {
   {user && (user.userRole === "Admin" || user.userRole === "Seller") && (
     <Link to="/products/quantity&Sales">
       <Button
-        type="primary"
-        className="bg-first text-white flex items-center justify-center h-12 w-20 bg-no-repeat bg-center bg-[length:50%] border-none hover:scale-110 transition-transform duration-300 ease-in-outt"
+        type="danger"
+        className="bg-second text-white hover:bg-black flex items-center justify-center bg-no-repeat bg-center bg-[length:55%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
       >
         Reports
       </Button>
@@ -206,8 +206,8 @@ const AdminProductGrid = ({ setFlag }) => {
   
   {user && (user.userRole === "Admin" || user.userRole === "Seller") && (
     <Button
-      type="primary"
-      className="bg-first text-white flex items-center justify-center h-12 w-20 bg-no-repeat bg-center bg-[length:50%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
+      type="danger"
+      className="bg-second text-white hover:bg-black flex items-center justify-center bg-no-repeat bg-center bg-[length:55%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
       onClick={() => setShowArchived(!showArchived)}
     >
       {showArchived ? "Archived" : "Active"}
@@ -215,7 +215,8 @@ const AdminProductGrid = ({ setFlag }) => {
   )}
 
 <Button
-  className="bg-first text-white flex items-center justify-center h-12 w-20 bg-no-repeat bg-center bg-[length:55%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
+type="danger"
+  className="bg-second text-white hover:bg-black flex items-center justify-center w-12  bg-no-repeat bg-center bg-[length:55%] border-none hover:scale-110 transition-transform duration-300 ease-in-out"
   style={{
     backgroundImage: `url(${cartIcon})`,
   }}
