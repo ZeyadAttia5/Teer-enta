c2a6899cd521eab7b2ffa509106868e843cb90ec
Yusuf <youssefmmagdy55@gmail.com>
2024-12-04 15:39:38 -0800

    promo code bar

--- frontend/src/components/shared/NavBar/TouristNavBar.jsx
+++ frontend/src/components/shared/NavBar/TouristNavBar.jsx
@@ -13,6 +13,7 @@ import logo from "../../../assets/logo/logo2.jpg";
 import NotificationIcon from "./notificationIcon";
 import { getCart } from "../../../api/cart.ts";
 import { getCurrency } from "../../../api/account.ts";
+import PromoCodeStrip from "../../PromoCodeAdmin/PromoCodeStrip.jsx";
 
 const SideBar = ({ children, classNames }) => {
   const size = useMediaQuery();
@@ -35,7 +36,7 @@ const SideBar = ({ children, classNames }) => {
   return children;
 };
 
-const TouristNavBar = ({ setModalOpen, isNavigate, setIsNavigate }) => {
+const TouristNavBar = ({ setModalOpen, isNavigate, setIsNavigate, setVisibleFlagHome }) => {
   const navigate = useNavigate();
   const size = useMediaQuery();
   const storedUser = localStorage.getItem("user");
@@ -87,167 +88,172 @@ const TouristNavBar = ({ setModalOpen, isNavigate, setIsNavigate }) => {
       fetchCartCount();
     }
   }, [isNavigate, user]);
-
+  const [visible, setVisible] = useState(true);
   return (
-    <div
-      className={`w-full fixed bg-white flex justify-between shadow-md  items-center to-teal-700%  p-6 h-20 text-white font-bold space-x-8`}
-    >
-      <div className="flex gap-4 ml-8">
-        {/* Logo Section */}
-        <span className="ml-16 text-lg leading-7">
-          <div className="cursor-pointer w-fit border border-transparent  p-2 rounded-md transition-all duration-300 hover:scale-105">
-            {/* Logo Link */}
-            <Link to={"/"} className="ring-0">
-              <img
-                src={logo}
-                alt="Logo"
-                width={80}
-                className="rounded-full shadow-lg hover:rotate-6 transition-all duration-500"
-              />
-            </Link>
-          </div>
-        </span>
-        <span className="text-first flex text-4xl justify-center items-center italic">TEER ENTA</span>
-        {/* Search Bar */}
-      </div>
-
-      {!["xs", "sm", "md"].includes(size) && (
-        <div className="flex items-center gap-5">
-          {user && user.userRole === "Tourist" && (
-            <>
-              {/* Bookings Icon */}
-              <div
-                onClick={() => navigate("/newProfile")}
-                className=" text-first hover:border-b-2 px-2 pt-2 mt-2 hover:border-first transition-all duration-300 transform cursor-pointer"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="gray"
-                  class="size-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
-                  />
-                </svg>
-
-                <span className="text-fifth text-sm">{currency?.code}</span>
-              </div>
-
-              <div
-                onClick={() => navigate("/Bookings")}
-                className=" text-first hover:border-b-2 px-2 pt-2 hover:border-first transition-all duration-300 transform cursor-pointer"
-              >
-                <CalendarOutlined
-                  className={`text-fifth text-2xl transition-colors block`}
+    <div className="fixed w-full">
+      <PromoCodeStrip setVisibleFlag={setVisible} setVisibleFlagHome={setVisibleFlagHome}/>
+      <div
+        className={`w-full fixed ${visible ? "top-11" : "top-0"} bg-white flex justify-between shadow-md  items-center to-teal-700%  p-6 h-20 text-white font-bold space-x-8`}
+      >
+        <div className="flex gap-4 ml-8">
+          {/* Logo Section */}
+          <span className="ml-16 text-lg leading-7">
+            <div className="cursor-pointer w-fit border border-transparent  p-2 rounded-md transition-all duration-300 hover:scale-105">
+              {/* Logo Link */}
+              <Link to={"/"} className="ring-0">
+                <img
+                  src={logo}
+                  alt="Logo"
+                  width={80}
+                  className="rounded-full shadow-lg hover:rotate-6 transition-all duration-500"
                 />
-                <span className="text-fifth text-sm">Calender</span>
-              </div>
+              </Link>
+            </div>
+          </span>
+          <span className="text-first flex text-4xl justify-center items-center italic">
+            TEER ENTA
+          </span>
+          {/* Search Bar */}
+        </div>
 
-              {/* Cart Icon */}
-              {user && user.userRole === "Tourist" && (
+        {!["xs", "sm", "md"].includes(size) && (
+          <div className="flex items-center gap-5">
+            {user && user.userRole === "Tourist" && (
+              <>
+                {/* Bookings Icon */}
                 <div
-                  onClick={() => navigate("/products/cart")}
-                  className=" text-first hover:border-b-2 px-2 pt-2 mt-1 hover:border-first transition-all duration-300 transform cursor-pointer"
+                  onClick={() => navigate("/newProfile")}
+                  className=" text-first hover:border-b-2 px-2 pt-2 mt-2 hover:border-first transition-all duration-300 transform cursor-pointer"
                 >
-                  <Badge
-                    count={cartCount}
-                    offset={[9, -5]}
-                    color={"red"}
-                    className="block"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="gray"
+                    class="size-6"
                   >
-                    <ShoppingCartOutlined
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+                    />
+                  </svg>
+
+                  <span className="text-fifth text-sm">{currency?.code}</span>
+                </div>
+
+                <div
+                  onClick={() => navigate("/Bookings")}
+                  className=" text-first hover:border-b-2 px-2 pt-2 hover:border-first transition-all duration-300 transform cursor-pointer"
+                >
+                  <CalendarOutlined
+                    className={`text-fifth text-2xl transition-colors block`}
+                  />
+                  <span className="text-fifth text-sm">Calender</span>
+                </div>
+
+                {/* Cart Icon */}
+                {user && user.userRole === "Tourist" && (
+                  <div
+                    onClick={() => navigate("/products/cart")}
+                    className=" text-first hover:border-b-2 px-2 pt-2 mt-1 hover:border-first transition-all duration-300 transform cursor-pointer"
+                  >
+                    <Badge
+                      count={cartCount}
+                      offset={[9, -5]}
+                      color={"red"}
+                      className="block"
+                    >
+                      <ShoppingCartOutlined
+                        className={`text-fifth text-2xl transition-colors`}
+                      />
+                    </Badge>
+                    <span className="text-fifth text-sm">Cart</span>
+                  </div>
+                )}
+              </>
+            )}
+
+            {/* Notifications Icon */}
+            {user &&
+              (user.userRole === "Tourist" ||
+                user.userRole === "Advertiser" ||
+                user.userRole === "TourGuide") && (
+                <div className=" text-first hover:border-b-2 px-2 hover:border-first transition-all duration-300 transform cursor-pointer">
+                  <div className="flex justify-center">
+                    <NotificationIcon
                       className={`text-fifth text-2xl transition-colors`}
                     />
-                  </Badge>
-                  <span className="text-fifth text-sm">Cart</span>
+                  </div>
+                  <span className="text-fifth text-sm">Notifications</span>
                 </div>
               )}
-            </>
-          )}
 
-          {/* Notifications Icon */}
-          {user &&
-            (user.userRole === "Tourist" ||
-              user.userRole === "Advertiser" ||
-              user.userRole === "TourGuide") && (
-              <div className=" text-first hover:border-b-2 px-2 hover:border-first transition-all duration-300 transform cursor-pointer">
-                <div className="flex justify-center">
-                  <NotificationIcon
-                    className={`text-fifth text-2xl transition-colors`}
-                  />
+            {/* Account and Logout Buttons */}
+            <div className="flex justify-end items-center lg:flex-1 mt-2  mr-16">
+              <AccountButton
+                extra_tw={` transition duration-300 px-2 pt-2 transform`}
+                onClick={onAccountClick}
+                setModalOpen={setModalOpen}
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Mobile Menu */}
+        {["xs", "sm", "md"].includes(size) && (
+          <SideBar
+            classNames={{
+              body: "bg-fourth text-white flex flex-col items-center p-4 rounded-lg shadow-lg",
+              header: "bg-fourth text-white font-bold text-lg p-4",
+            }}
+          >
+            {user && user.userRole === "Tourist" && (
+              <div className="flex flex-col gap-4 w-full items-center mb-4">
+                {/* Mobile Bookings Icon */}
+                <div
+                  onClick={() => navigate("/Bookings")}
+                  className="flex items-center justify-center p-2 bg-fourth text-first hover:bg-second rounded-full shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12"
+                >
+                  <CalendarOutlined className="text-2xl" />
                 </div>
-                <span className="text-fifth text-sm">Notifications</span>
+                {/* Mobile Cart Icon */}
+                <div
+                  onClick={() => navigate("/products/cart")}
+                  className="flex items-center justify-center p-2 bg-fourth text-first hover:bg-second rounded-full shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12"
+                >
+                  <Badge count={cartCount} offset={[-5, 5]}>
+                    <ShoppingCartOutlined className="text-2xl" />
+                  </Badge>
+                </div>
+                {/* Mobile Notifications Icon */}
+                <div className=" text-first hover:border-b-2 hover:border-first shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12">
+                  <NotificationIcon />
+                  <span className="text-black">Notifications</span>
+                </div>
               </div>
             )}
 
-          {/* Account and Logout Buttons */}
-          <div className="flex justify-end items-center lg:flex-1 mt-2  mr-16">
-            <AccountButton
-              extra_tw={` transition duration-300 px-2 pt-2 transform`}
-              onClick={onAccountClick}
-              setModalOpen={setModalOpen}
-            />
-          </div>
-        </div>
-      )}
-
-      {/* Mobile Menu */}
-      {["xs", "sm", "md"].includes(size) && (
-        <SideBar
-          classNames={{
-            body: "bg-fourth text-white flex flex-col items-center p-4 rounded-lg shadow-lg",
-            header: "bg-fourth text-white font-bold text-lg p-4",
-          }}
-        >
-          {user && user.userRole === "Tourist" && (
-            <div className="flex flex-col gap-4 w-full items-center mb-4">
-              {/* Mobile Bookings Icon */}
-              <div
-                onClick={() => navigate("/Bookings")}
-                className="flex items-center justify-center p-2 bg-fourth text-first hover:bg-second rounded-full shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12"
-              >
-                <CalendarOutlined className="text-2xl" />
-              </div>
-              {/* Mobile Cart Icon */}
-              <div
-                onClick={() => navigate("/products/cart")}
-                className="flex items-center justify-center p-2 bg-fourth text-first hover:bg-second rounded-full shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12"
-              >
-                <Badge count={cartCount} offset={[-5, 5]}>
-                  <ShoppingCartOutlined className="text-2xl" />
-                </Badge>
-              </div>
-              {/* Mobile Notifications Icon */}
-              <div className=" text-first hover:border-b-2 hover:border-first shadow-md transition-all duration-300 transform hover:scale-110 cursor-pointer w-12 h-12">
-                <NotificationIcon />
-                <span className="text-black">Notifications</span>
-              </div>
+            <div className="flex justify-end items-center lg:flex-1 mt-4">
+              <AccountButton
+                extra_tw="bg-fourth text-first hover:bg-second transition duration-300 p-2 rounded-lg shadow-lg flex items-center justify-center transform hover:scale-110"
+                onClick={onAccountClick}
+              />
             </div>
-          )}
 
-          <div className="flex justify-end items-center lg:flex-1 mt-4">
-            <AccountButton
-              extra_tw="bg-fourth text-first hover:bg-second transition duration-300 p-2 rounded-lg shadow-lg flex items-center justify-center transform hover:scale-110"
-              onClick={onAccountClick}
-            />
-          </div>
-
-          {user && (
-            <button
-              onClick={() => setModalOpen(true)}
-              className="flex items-center text-first hover:text-white justify-center mt-4 p-2 bg-fourth hover:bg-red-700 rounded-full transition duration-300 shadow-lg transform hover:scale-110 focus:outline-none"
-              aria-label="Logout"
-            >
-              <FiLogOut className="w-6 h-6" />
-            </button>
-          )}
-        </SideBar>
-      )}
+            {user && (
+              <button
+                onClick={() => setModalOpen(true)}
+                className="flex items-center text-first hover:text-white justify-center mt-4 p-2 bg-fourth hover:bg-red-700 rounded-full transition duration-300 shadow-lg transform hover:scale-110 focus:outline-none"
+                aria-label="Logout"
+              >
+                <FiLogOut className="w-6 h-6" />
+              </button>
+            )}
+          </SideBar>
+        )}
+      </div>
     </div>
   );
 };
